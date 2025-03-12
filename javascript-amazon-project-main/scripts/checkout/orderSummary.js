6119cf8e8c1bf6c1e12422a464e4c675c8c5c9f2
imyuvi0 <jk0424366@gmail.com>
2025-03-12 22:20:34 +0530

    add jasmine testing framework

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -11,9 +11,7 @@ import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary(){
 
-
 let cartSummaryHTML='';
-
 
 cart.forEach((cartItem)=>{
   const productId = cartItem.productId;
